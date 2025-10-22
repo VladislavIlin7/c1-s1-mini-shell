436c99db3fc260df79f0b6af4c618be43c4ddeae
VladislavIlin7 <svladilin@gmail.com>
2025-10-22 22:36:37 +0300

    add simple command ls

---
+++ src/commands/cmd_ls.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+
+
+def resolve(path: str) -> Path:
+    current_cwd = Path.cwd()
+    p = Path(path)
+    return p if p.is_absolute() else current_cwd / p
+
+def cmd_ls(args: list[str]):
+    current_cwd = Path.cwd()
+    target = resolve(args[1]) if len(args) > 1 else current_cwd
+
+    if not target.exists():
+        print("Нет такой папки")
+        return
+
+    if target.is_dir():
+        items = list(target.iterdir())
+        if not items:
+            print("Папка пуста")
+        else:
+            for item in items:
+                print(item.name)
+    else:
+        print("Это не папка")
--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
-from src.power import power_function
-from src.constants import SAMPLE_CONSTANT
+import os
+from pathlib import Path
+
+from src.commands.cmd_ls import cmd_ls
 
 
-def main() -> None:
-    """
-    Обязательнная составляющая программ, которые сдаются. Является точкой входа в приложение
-    :return: Данная функция ничего не возвращает
-    """
+def main():
+    start_path = r'C:\Users\Public'  # начальный каталог
+    os.chdir(start_path)
 
-    target, degree = map(int, input("Введите два числа разделенные пробелом: ").split(" "))
-
-    result = power_function(target=target, power=degree)
-
-    print(result)
-
-    print(SAMPLE_CONSTANT)
+    while True:
+        current_cwd = Path.cwd()
+        cmd_input: str = input(f'{current_cwd}> ')
+        if cmd_input.split()[0] == 'ls':
+            cmd_ls(cmd_input.split())
 
 if __name__ == "__main__":
     main()
