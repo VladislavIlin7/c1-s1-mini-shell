02d53f8bd8efb7eb4ec171d6b4326cc3d08568d1
VladislavIlin7 <svladilin@gmail.com>
2025-10-28 14:12:12 +0300

    add -l in cmd_ls

--- src/commands/cmd_ls.py
+++ src/commands/cmd_ls.py
@@ -1,3 +1,6 @@
+import datetime
+import os
+import stat
 from pathlib import Path
 
 from src.commands.absolute_or_relative import absolute_or_relative
@@ -5,7 +8,9 @@ from src.commands.absolute_or_relative import absolute_or_relative
 
 def cmd_ls(args: list[str]):
     current_cwd = Path.cwd()
-    target = absolute_or_relative(args[1]) if len(args) > 1 else current_cwd
+    show_long = '-l' in args
+    paths = [arg for arg in args[1:] if not arg.startswith('-')]
+    target = absolute_or_relative(paths[0]) if paths else current_cwd
 
     if not target.exists():
         print("Нет такой папки")
@@ -15,10 +20,26 @@ def cmd_ls(args: list[str]):
         items = list(target.iterdir())
         if not items:
             print("Папка пуста")
-        else:
-            for item in items:
-                if item.name.startswith('.') or item.name == 'desktop.ini':
-                    continue
+            return
+
+        if show_long:
+            print(f"\n{target}>")
+            print(f"{'MODE':<11} {'SIZE':>10} {'LAST MODIFIED':<17} NAME")
+
+        for item in items:
+            if item.name.startswith('.') or item.name == 'desktop.ini':
+                continue
+
+            if show_long:
+                try:
+                    st = os.stat(item)
+                    mode = stat.filemode(st.st_mode)
+                    size = st.st_size
+                    mtime = datetime.datetime.fromtimestamp(st.st_mtime).strftime('%Y-%m-%d %H:%M')
+                    print(f"{mode} {size:>10} {mtime} {item.name}")
+                except Exception as e:
+                    print(f"Ошибка при доступе к {item}: {e}")
+            else:
                 print(item.name)
     else:
         print("Это не папка")
